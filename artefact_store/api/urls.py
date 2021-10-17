6266a24a33226a15ca19faeb57c081f302c67687
antoine redier <antoine.redier@hec.edu>
2021-10-17 11:07:06 +0200

    basic apps and dapr connextion

---
+++ artefact_store/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import include, path
+
+from . import views
+
+urlpatterns = [
+    path("dummy_view", views.DummyView.as_view()),
+    path("related_view", views.DummyConnectedView.as_view()),
+]
--- workflow_controller/workflow_controller/urls.py
+++ workflow_controller/workflow_controller/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django import urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    urls.path('admin/', admin.site.urls),
+    urls.path("api/", urls.include("api.urls"))
 ]
