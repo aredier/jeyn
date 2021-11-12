5daa9ec7be714a0d1f4830688a02415f9965347c
antoine <aredier@smartadserver.com>
2021-11-12 18:28:56 +0100

    postgress deployment

--- jeyn_core/artefact_store/artefact_store/settings.py
+++ jeyn_core/artefact_store/artefact_store/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'artefact_store.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("POSTGRES_DB", ""),
+        'USER': os.environ.get("POSTGRES_USER", ""),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': 'postgres.default.svc.cluster.local',
+        'PORT': '5432',
     }
 }
 
