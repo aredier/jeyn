d6723d2e1c63c9a16de27290a7746dd662487c95
antoine redier <antoine.redier@hec.edu>
2021-11-20 19:21:41 +0100

    databases

--- jeyn_core/workflow_controller/workflow_controller/settings.py
+++ jeyn_core/workflow_controller/workflow_controller/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'workflow_controller.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("POSTGRES_DB", ""),
+        'USER': os.environ.get("POSTGRES_USER", ""),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': 'postgres.default.svc.cluster.local',
+        'PORT': '5432',
     }
 }
 
